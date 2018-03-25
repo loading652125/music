29bcfc9a93eafb9a1e25c58580847cd5fac8429d
loading652125 <loading652125@163.com>
2018-03-25 15:29:54 +0800

    请求接口类型代码

---
+++ app/src/main/java/com/ldrtech/music/constants/RequestCode.java
@@ -0,0 +1,11 @@
+package com.ldrtech.music.constants;
+
+/**
+ * Created by xiaoh on 2018/3/25.
+ */
+
+public interface RequestCode {
+    int REQUEST_WRITE_SETTINGS = 0;
+    int REQUEST_ALBUM = 1;
+    int REQUEST_CORP = 2;
+}
